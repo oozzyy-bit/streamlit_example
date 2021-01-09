91a79784330f9ff7f0f47ed6188b4ee8fa423839
oozzyy-bit <71003813+oozzyy-bit@users.noreply.github.com>
2021-01-09 17:17:44 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+# Simple Stock Price App
+Shown are the stock closing price and volume of Google!
+""")
+
+
+ticker_symbol = 'GOOGL'
+ticker_data = yf.Ticker(ticker_symbol)
+ticker_df = ticker_data.history(period='1d', start='2010-5-31', en='2020-5-31')
+
+st.write("""## Closing Price""")
+st.line_chart(ticker_df.Close)
+
+st.write("""## Volume""")
+st.line_chart(ticker_df.Volume)
