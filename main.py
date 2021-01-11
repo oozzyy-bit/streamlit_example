f8aac912afc3b036865b93ed6b894b981810f36c
oozzyy-bit <atakano@itu.edu.tr>
2021-01-11 14:09:17 +0300

    init nyc example

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-import yfinance as yf
+import pandas as pd
 import streamlit as st
+import plotly.express as px
 
-st.write("""
-# Simple Stock Price App
-Shown are the stock closing price and volume of Google!
-""")
+@st.cache
+def get_data():
+    url = "http://data.insideairbnb.com/united-states/ny/new-york-city/2019-09-12/visualisations/listings.csv"
+    return pd.read_csv(url)
+df = get_data()
 
+st.title("Streamlit NYC Airbnb Deneme")
+st.markdown("Airbnb datası üzerinde oynanmaya çalışılacak.")
 
-ticker_symbol = 'GOOGL'
-ticker_data = yf.Ticker(ticker_symbol)
-ticker_df = ticker_data.history(period='1d', start='2010-5-31', en='2020-5-31')
+st.dataframe(df.head())
 
-st.write("""## Closing Price""")
-st.line_chart(ticker_df.Close)
-
-st.write("""## Volume""")
-st.line_chart(ticker_df.Volume)
+cols = ["name", "host_name", "neighbourhood", "room_type", "price"]
+st_ms = st.multiselect("Columns", df.columns.tolist(), default=cols)
